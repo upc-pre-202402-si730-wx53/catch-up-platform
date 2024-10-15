c5c3ba799e223b0c86e3e37d5d5d3ad441a519b6
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-14 20:23:45 -0500

    feat(orm): add orm naming convention extensions

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions
+{
+    public static class ModelBuilderExtension
+    {
+        public static void UseSnakeCaseNamingConvention(this ModelBuilder builder)
+        {
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                var tableName = entity.GetTableName();
+                if (!string.IsNullOrEmpty(tableName)) entity.SetTableName(tableName.ToSnakeCase());
+
+                foreach(var property in entity.GetProperties())
+                    property.SetColumnName(property.GetColumnName().ToSnakeCase());
+
+                foreach (var key in entity.GetKeys())
+                {
+                    var keyName = key.GetName();
+                    if (!string.IsNullOrEmpty(keyName)) key.SetName(keyName.ToSnakeCase());
+                }
+                foreach (var foreignKey in entity.GetForeignKeys())
+                {
+                    var foreignKeyName = foreignKey.GetConstraintName();
+                    if (!string.IsNullOrEmpty(foreignKeyName)) foreignKey.SetConstraintName(foreignKeyName.ToSnakeCase());
+                }
+
+                foreach(var index in entity.GetIndexes())
+                {
+                    var indexName = index.GetDatabaseName();
+                    if (!string.IsNullOrEmpty(indexName)) index.SetDatabaseName(indexName.ToSnakeCase());
+                }   
+
+            }
+        }
+    }
+}
