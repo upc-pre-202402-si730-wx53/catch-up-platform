58d8f3976d358fa770cae53390ccf26d8f141476
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-14 20:24:13 -0500

    feat(orm): add base repository and unit of work contracts

---
+++ CatchUpPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CatchUpPlatform.API.Shared.Domain.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        Task AddAsync(TEntity entity);
+        Task<TEntity?> FindByAsync(int id);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<IEnumerable<TEntity>> ListAsync();
+    }
+}
---
+++ CatchUpPlatform.API/Shared/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace CatchUpPlatform.API.Shared.Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync();
+    }
+}
