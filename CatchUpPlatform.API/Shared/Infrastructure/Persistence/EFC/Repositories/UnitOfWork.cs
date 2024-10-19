5ff7c203be2ad94c600747b7cb3d89908853751b
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-19 10:53:00 -0500

    feat(orm): add configuration and base classes implementation

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
